9237c71dc9d79b918efc1c15ce45e7bc1b7bbfe2
ttin9 <skj974600@gmail.com>
2023-11-14 15:05:25 +0900

    add admin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import CategorySection from "./_components/CategorySection";
+import ProductionSection from "./_components/ProductionSection";
+import ProductSection from "./_components/ProductSection";
+
+export default function Admin() {
+  return (
+    <div>
+      <CategorySection />
+      <ProductionSection />
+      <ProductSection />
+    </div>
+  );
+}
