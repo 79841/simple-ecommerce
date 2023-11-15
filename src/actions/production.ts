95a4cbf01f640557f31783a9b488357d508f4bf2
LEtMeDEv <79841@naver.com>
2023-11-16 05:19:53 +0900

    add production actions

---
+++ src/actions/production.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import prisma from "@/prisma/client";
+import { TCreateProduction, TProduction } from "@/types/Production";
+import { revalidatePath } from "next/cache";
+
+export const createProduction = async (production: TCreateProduction) => {
+  await prisma.production.create({
+    data: production,
+  });
+  revalidatePath("/admin");
+};
+
+export const getProductions = async () => {
+  const productions = await prisma.production.findMany({});
+  return productions;
+};
+
+export const searchProductions = async (keyword: string) => {
+  const productions = await prisma.production.findMany({
+    where: {
+      name: {
+        contains: keyword,
+      },
+    },
+  });
+  return productions;
+};
+
+export const deleteProduction = async (productionId: TProduction["id"]) => {
+  await prisma.production.delete({
+    where: {
+      id: productionId,
+    },
+  });
+  await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+  revalidatePath("admin");
+};
