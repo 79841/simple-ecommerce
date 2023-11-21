fd11af55dae250e79cd1d456c06f18302788b8bf
LEtMeDEv <79841@naver.com>
2023-11-21 15:32:46 +0900

    add add button

---
+++ src/components/shared/button/AddButton.tsx
@@ -0,0 +1,12 @@
+const AddButton = () => {
+  return (
+    <button
+      type="submit"
+      className="border-black border-2 border-solid h-8 rounded-md w-16"
+    >
+      Add
+    </button>
+  );
+};
+
+export default AddButton;
