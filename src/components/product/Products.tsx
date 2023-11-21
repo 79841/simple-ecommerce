d9cc71c614b89e6a302fdd18691e81a5c528bbfa
LEtMeDEv <79841@naver.com>
2023-11-21 15:32:10 +0900

    add products

--- src/components/product/Products.tsx
+++ src/components/product/Products.tsx
@@ -1,3 +1,15 @@
-export const Products = () => {
-  return <></>;
+import { getProducts } from "@/actions/product";
+import { Product } from ".";
+
+export const Products = async () => {
+  const products = await getProducts();
+  return (
+    <div className="w-[70%]">
+      <ul>
+        {products.map((product) => (
+          <Product key={product.id} item={product} />
+        ))}
+      </ul>
+    </div>
+  );
 };
