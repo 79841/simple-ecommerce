85e3be8c0188723028dcd771b9b126edce6ec935
LEtMeDEv <79841@naver.com>
2023-11-21 15:32:00 +0900

    add product

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,3 +1,28 @@
-export const Product = () => {
-  return <></>;
+"use client";
+import { deleteProduct } from "@/actions/product";
+import { TProduct } from "@/types/Product";
+import { z } from "zod";
+import DeleteForm from "../shared/form/DeleteForm";
+type TProductProps = {
+  item: TProduct;
+};
+export const Product = ({ item }: TProductProps) => {
+  const formAction = async (formData: FormData) => {
+    const schema = z.object({
+      id: z.string().min(1),
+    });
+    const validatedData = schema.safeParse({
+      id: formData.get("id"),
+    });
+    if (!validatedData.success) {
+      return { message: "Wrong product id" };
+    }
+    try {
+      await deleteProduct(validatedData.data.id);
+      return { message: `Deleted product ${item.name}` };
+    } catch (e) {
+      return { mesage: "Falied to delete product" };
+    }
+  };
+  return <DeleteForm formAction={formAction} item={item} />;
 };
