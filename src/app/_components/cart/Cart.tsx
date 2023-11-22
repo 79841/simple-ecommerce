3f7e204c8243c0bc33643bb3ddce650bdcf74fe9
LEtMeDEv <79841@naver.com>
2023-11-22 21:43:18 +0900

    add cart section

---
+++ src/app/_components/cart/Cart.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { addItem, deleteItem, subItem } from "@/lib/redux";
+import { cartSelector } from "@/lib/redux/slices/cartSlice/selector";
+import { TProduct } from "@/types/Product";
+import { MouseEventHandler } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+type TCartItemProps = {
+  item: { count: number; product: TProduct };
+};
+const CartItem = ({ item }: TCartItemProps) => {
+  const { count, product } = item;
+  const dispatch = useDispatch();
+
+  const handleMinus: MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(subItem(product.id));
+  };
+  const handlePlus: MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(addItem({ count: 1, product }));
+  };
+
+  const handleDelete: MouseEventHandler<HTMLButtonElement> = () => {
+    dispatch(deleteItem(product.id));
+  };
+  return (
+    <div className="rounded-md border-2 border-solid border-black p-2">
+      <div>{product.name}</div>
+      <div>{count}</div>
+      <div>{product.price}</div>
+      <div>
+        <button className="button-bordered" onClick={handleMinus}>
+          -
+        </button>
+        <div>count</div>
+        <button className="button-bordered" onClick={handlePlus}>
+          +
+        </button>
+      </div>
+      <button className="button-bordered" onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export const Cart = () => {
+  const cart = useSelector(cartSelector);
+  const totalAmount = Object.values(cart).reduce(
+    (total, { count, product }) => {
+      return total + count * product.price;
+    },
+    0,
+  );
+  return (
+    <div>
+      {Object.values(cart).map((item) => (
+        <CartItem key={item.product.id} item={item} />
+      ))}
+      <div>{totalAmount}</div>
+    </div>
+  );
+};
